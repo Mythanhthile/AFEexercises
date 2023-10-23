1d531f1662bca338501d2885bc3832be28a144a5
Mythanhthile <93250566+Mythanhthile@users.noreply.github.com>
2023-10-23 14:04:30 +0200

    yessir

---
+++ lektion3/lesson03-network-communication/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'lesson03-network-communication'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('lesson03-network-communication');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lesson03-network-communication app is running!');
+  });
+});
---
+++ lektion3/lesson03-network-communication/src/app/client/client-overview/client-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-overview',
+  templateUrl: './client-overview.component.html',
+  styleUrls: ['./client-overview.component.scss']
+})
+export class ClientOverviewComponent {
+
+}
